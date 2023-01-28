a478b2b79eaabdad7aa5e20cf18481c98d28f801
DrinKurtishi <kurtishidrin@gmail.com>
2023-01-28 13:09:19 +0100

    Slider now changes grid

--- etch.js
+++ etch.js
@@ -1,5 +1,26 @@
-let rows = 128;
-let columns = 128;
+let slider = document.getElementById('mySlider');
+let rows = 32;
+let columns = 32;
+slider.addEventListener("change", () => {
+    rows = slider.value;
+    columns = slider.value;
+    let grid = document.getElementById("grid");
+    grid.innerHTML = "";// TODO clear previous grid 
+
+for (let i = 0; i < columns; i++)
+    {
+        let column = document.createElement('div');//creates columns
+        column.className = 'column';
+        for(let j = 0; j < rows; j++)
+        {
+            let row = document.createElement('div');
+            row.className = 'row';
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }
+});
+
 let grid = document.getElementById("grid");
 for (let i = 0; i < columns; i++)
     {
